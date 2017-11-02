97460a125579d2c8f46a0886de1b48f17ccaed6c
Zack Rock <zacharyrock@hotmail.com>
2017-11-02 00:23:51 -0700

    init

---
+++ Assets/GameManager.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour {
+
+    public GameObject crexModel;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void StartGame()
+    {
+        GameObject.Find("Canvas/TitleCard").gameObject.SetActive(false);
+        crexModel.SetActive(true);
+    }
+}
